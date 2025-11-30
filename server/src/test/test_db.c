e2d2acb7d9502936e9cd86e9d68ad8772a8f144a
Phan Sỹ Hùng <hungphan120304@gmail.com>
2025-11-30 12:26:59 +0700

    change test

--- server/src/test/test_db.c
+++ server/src/test/test_db.c
@@ -1,23 +1,62 @@
-// server/src/test/test_db.c
 #include <stdio.h>
-#include "db.h"
+#include <stdlib.h>
+#include <libpq-fe.h>
 
 int main(void) {
-    const char *conninfo = getenv("DB_CONN");
-    if (!conninfo) {
-        fprintf(stderr, "Please set DB_CONN env, ví dụ:\n");
-        fprintf(stderr,
-          "export DB_CONN=\"host=localhost port=5432 dbname=ltm_group04 user=postgres password=1\"\n");
-        return 1;
+    const char *conninfo =
+        "host=127.0.0.1 port=5432 dbname=ltm_group04 user=postgres password=1";
+
+    printf("Connecting with conninfo: %s\n", conninfo);
+
+    PGconn *conn = PQconnectdb(conninfo);
+
+    if (conn == NULL) {
+        fprintf(stderr, "[FATAL] PQconnectdb returned NULL (libpq problem)\n");
+        return EXIT_FAILURE;
     }
 
-    if (db_connect(conninfo) != 0) return 1;
+    ConnStatusType status = PQstatus(conn);
+    if (status != CONNECTION_OK) {
+        fprintf(stderr, "[ERROR] Connection failed: %s\n", PQerrorMessage(conn));
+        PQfinish(conn);
+        return EXIT_FAILURE;
+    }
 
-    if (db_is_ok())
-        printf("DB OK\n");
-    else
-        printf("DB NOT OK\n");
+    printf("[OK] Connected to database.\n");
 
-    db_disconnect();
-    return 0;
+    // Test query
+    const char *sql = "SELECT 1;";
+    printf("Running test query: %s\n", sql);
+
+    PGresult *res = PQexec(conn, sql);
+
+    if (res == NULL) {
+        fprintf(stderr, "[ERROR] PQexec returned NULL: %s\n", PQerrorMessage(conn));
+        PQfinish(conn);
+        return EXIT_FAILURE;
+    }
+
+    ExecStatusType rstatus = PQresultStatus(res);
+    if (rstatus != PGRES_TUPLES_OK) {
+        fprintf(stderr, "[ERROR] Query failed: %s\n", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        return EXIT_FAILURE;
+    }
+
+    int nrows = PQntuples(res);
+    int ncols = PQnfields(res);
+
+    printf("[OK] Query succeeded. Rows=%d, Cols=%d\n", nrows, ncols);
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            printf("row %d col %d = %s\n", i, j, PQgetvalue(res, i, j));
+        }
+    }
+
+    PQclear(res);
+    PQfinish(conn);
+
+    printf("[DONE] Test finished successfully.\n");
+    return EXIT_SUCCESS;
 }
